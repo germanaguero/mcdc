2aa820621055ace39128019d6e23153f0480e960
german aguero <germanaguero@hotmail.com>
2013-03-22 08:15:39 -0300

    change html and css on header.php
    change html and css on header.php

--- includes/header.php
+++ includes/header.php
@@ -2,17 +2,22 @@
 
 ?>
 <!--<a href="<?php echo BASE_PATH; ?>"><img src ="<?php echo IMAGE_PATH_ICO; ?>logo_short2.png" alt="logo" title="logo"></a>-->
-<a id="logo_link" href="<?php echo BASE_PATH; ?>">MCDC</a>
-<div id="container">
-  <div id="mainContent">
+
+<div id="logo_container">
+	<a id="logo_link" href="<?php echo BASE_PATH; ?>">MCDC</a>
+
     <div id="searchBox">
 		<form method="get" action="#">
         	<input id="searchBoxInput" type="text" value="Nombre de Pelicula o Disco" class="search" onclick="clearSearchBoxInput();">
             <input id="searchBoxButton" type="submit" value="Buscar" class="submit" onclick="search();">
        	</form> 
     </div>
+
 	<div id="response_ajax"></div>
+	
 </div>
+
+
 <?php
 if($server_request == "90.0.4.143"){
 echo "<div id=\"horario\">
